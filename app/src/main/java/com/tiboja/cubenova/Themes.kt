870d3da30a887c8cfe5dabb911adea652e4f6b02
janintibo-art <ton-email@exemple.com>
2026-07-12 04:33:40 +0200

    5 nouveaux themes : Espace, Espace fun, Abstrait, Kawaii, Delire

--- app/src/main/java/com/tiboja/cubenova/Themes.kt
+++ app/src/main/java/com/tiboja/cubenova/Themes.kt
@@ -4,9 +4,9 @@ object Themes {
     val NAMES = arrayOf(
         "Classique", "Mangas", "Elfes", "Yggdrasil", "Zen", "Punk",
         "Smileys", "Jungle", "Saisons", "Or", "Émeraude", "Crépuscule",
-        "Nature"
+        "Nature", "Espace", "Espace fun", "Abstrait", "Kawaii", "Délire"
     )
-    val ATLAS = Array(13) { "atlas/theme_%02d.jpg".format(it) }
-    val THUMBS = Array(13) { "thumbs/theme_%02d.jpg".format(it) }
+    val ATLAS = Array(18) { "atlas/theme_%02d.jpg".format(it) }
+    val THUMBS = Array(18) { "thumbs/theme_%02d.jpg".format(it) }
     const val DEFAULT = 0
 }
