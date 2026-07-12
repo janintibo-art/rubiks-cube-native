9ff07f6f99fb057c074c14218eac4dcf80ca6124
janintibo-art <ton-email@exemple.com>
2026-07-12 02:03:35 +0200

    Nouveau theme Nature + succes faces + themes gemmes + ombre

--- app/src/main/java/com/tiboja/cubenova/Themes.kt
+++ app/src/main/java/com/tiboja/cubenova/Themes.kt
@@ -3,9 +3,10 @@ package com.tiboja.cubenova
 object Themes {
     val NAMES = arrayOf(
         "Classique", "Mangas", "Elfes", "Yggdrasil", "Zen", "Punk",
-        "Smileys", "Jungle", "Saisons", "Or", "Émeraude", "Crépuscule"
+        "Smileys", "Jungle", "Saisons", "Or", "Émeraude", "Crépuscule",
+        "Nature"
     )
-    val ATLAS = Array(12) { "atlas/theme_%02d.jpg".format(it) }
-    val THUMBS = Array(12) { "thumbs/theme_%02d.jpg".format(it) }
+    val ATLAS = Array(13) { "atlas/theme_%02d.jpg".format(it) }
+    val THUMBS = Array(13) { "thumbs/theme_%02d.jpg".format(it) }
     const val DEFAULT = 0
 }
